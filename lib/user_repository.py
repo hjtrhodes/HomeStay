ce962ab189ba64ad51f7fc91bd4c518c9ee95aac
rasianabrown@gmail.com <rasianabrown@gmail.coml>
2023-10-25 12:02:45 +0100

    user_repository_complete

--- lib/user_repository.py
+++ lib/user_repository.py
@@ -8,4 +8,19 @@ class UserRepository():
         rows = self._connection.execute('SELECT * from users WHERE id = %s', [id])
         row = rows[0]
         return User(row['id'], row['user_name'], row['user_email'], row['user_password'])
+
+    def get_by_email(self, email):
+        rows = self._connection.execute('SELECT * from users WHERE user_email = %s', [email])
+        for row in rows:
+            if email == row['user_email']:
+                return User(row['id'], row['user_name'], row['user_email'], row['user_password'])
+            else:
+                return False
+
+    def validate_credentials(self, email, password):
+        user = self.get_by_email(email)
         
+        if user:
+            if user.user_password == password:
+                return True
+        return False
