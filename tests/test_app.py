20926c9e9c452c00b82643ebb37cd21f0d4b1310
Harry Rhodes <hjtrhodes@gmail.com>
2023-10-25 14:53:29 +0100

    Login routes completed

--- tests/test_app.py
+++ tests/test_app.py
@@ -30,3 +30,26 @@ def test_get_login(page, test_web_address):
     # We assert that it has the text "This is the homepage."
     expect(form_tag).to_have_text("Enter email:")
 
+
+"""
+Test for login form
+"""
+# def test_submit_login_form(page, db_connection, web_client):
+#     page.set_default_timeout(1000)
+#     db_connection.seed('seeds/makers_bnb_seed.sql')
+#     post_response = web_client.post('/login', data={'email':'bjohnson@email.com','password':'mysecretpassword'})
+#     page.click("text='Login'")
+#     assert post_response.status_code == 200
+#     get_response=web_client.get('/spaces')
+#     assert get_response.status_code == 200
+#     p_tag = page.locator('p')
+#     expect(p_tag).to_have_text("Spaces")
+
+
+# def test_login_post_valid_credentials(web_client, db_connection, page):
+#     db_connection.seed('seeds/makers_bnb_seed.sql')
+#     response = web_client.post('/login', data={'email':'bjohnson@email.com','password':'mysecretpassword'})
+#     page.screenshot(path="screenshot.png", full_page=True)
+#     assert response.status_code == 302
+#     assert 'Spaces' in response.location
+
